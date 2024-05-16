513114d59d890c2b48d0a6ea1629c6406483c239
NipuniBhagya <nipunib@wso2.com>
2024-05-16 10:51:34 +0530

    feat:add home and landing pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+    return (
+        <div className='container-center'>
+            <h1>Welcome to the Home Page!</h1>
+            <p>This is the content of the home page.</p>
+        </div>
+    );
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,26 @@
+import viteLogo from "../../public/vite.svg";
+import reactLogo from "../assets/react.svg";
+// import { useNavigate } from "react-router-dom";
+
+export const LandingPage = () => {
+    
+    // const navigate = useNavigate();
+
+    return (
+        <div className='container-center'>
+            <div>
+                <a href='https://vitejs.dev' target='_blank'>
+                    <img src={viteLogo} className='logo' alt='Vite logo' />
+                </a>
+                <a href='https://react.dev' target='_blank'>
+                    <img src={reactLogo} className='logo react' alt='React logo' />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            {/* <div className='button-container'>
+                <button onClick={ () => navigate("/login") }>Sign In</button>
+                <button onClick={ () => navigate("/signup") }>Create Account</button>
+            </div> */}
+        </div>
+    );
+};
